a94c9656c30a2b2aad31edae11a7b8a9fd34df13
TatianaRohovets <tnr78@ukr.net>
2023-04-07 19:28:46 +0300

    var1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import profileData from '../data/user';
+import data from '../data/data';
+import friends from '../data/friends';
+import transactions from '../data/transactions';
+
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import Container from './Container/Container';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+   <Container>
+    <Profile
+        username={profileData.username}
+        tag={profileData.tag}
+        location={profileData.location}
+        avatar={profileData.avatar}
+        stats={profileData.stats}
+      />
+    <Statistics title="Upload stats" stats={data} /> 
+    <FriendList friends={friends}/>
+    <TransactionHistory transactions={transactions}/>
+   </Container>
   );
 };
